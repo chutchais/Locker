749cc22a2d8b9f2c7ddc5b8147c6a18e55c74b99
chutchais <chutchai.s@gmail.com>
2016-09-12 21:19:40 +0700

    Update setup.sh
    Test

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+#Test git pull Sep 12,2016
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
